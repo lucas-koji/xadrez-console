87cdf32ab13873dedcfdc3d88d1ea477330fe6aa
Lucas Koji <furukawa.lucaskoji@gmail.com>
2024-03-02 20:45:20 -0300

    Comecando a colocar pecas no tabuleiro

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -12,7 +12,8 @@ namespace xadrez_console
             {
                 for (int j = 0; j < tab.colunas; j++)
                 {
-                    if (tab.peca(i, j)==null) {
+                    if (tab.peca(i, j) == null)
+                    {
                         Console.Write("- ");
                     }
                     else
--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -1,5 +1,4 @@
-﻿using xadrez_console.tabuleiro;
-
+﻿
 namespace tabuleiro
 {
     internal class Tabuleiro
@@ -18,6 +17,12 @@ namespace tabuleiro
         public Peca peca(int linha, int coluna)
         {
             return pecas[linha, coluna];
+        }
+
+        public void colocarPeca(Peca p, Posicao pos)
+        {
+            pecas[pos.linha, pos.coluna] = p;
+            p.posicao = pos;
         }
     }
 }
